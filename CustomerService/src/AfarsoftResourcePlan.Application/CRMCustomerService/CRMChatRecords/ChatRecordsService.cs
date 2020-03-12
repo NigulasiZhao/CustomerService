e9aaa90753ca208851ff10568ff83288d7e977a8
zhaoliang <185653517@qq.com>
2020-03-12 17:30:59 +0800

    已修改        CustomerService/src/AfarsoftResourcePlan.Application/AfarsoftResourcePlan.Application.csproj Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/ChatRecordsService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/IChatRecordsService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMCustomerConnect/CustomerConnectService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMCustomerConnect/ICustomerConnectService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMServiceConnect/IServiceConnectService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMServiceConnect/ServiceConnectService.cs

---
+++ CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/ChatRecordsService.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using AfarsoftResourcePlan.CustomerService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfarsoftResourcePlan.CRMCustomerService.CRMChatRecords
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ChatRecordsService : ApplicationService, IChatRecordsService
+    {
+        /// <summary>
+        /// 聊天记录表
+        /// </summary>
+        private readonly IRepository<ChatRecords, int> _chatRecords;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="chatRecords">聊天记录表</param>
+        public ChatRecordsService(IRepository<ChatRecords, int> chatRecords)
+        {
+            _chatRecords = chatRecords;
+        }
+
+        public string GetChatRecords()
+        {
+            return "";
+        }
+    }
+}
---
+++ CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMServiceConnect/ServiceConnectService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfarsoftResourcePlan.CRMCustomerService.CRMServiceConnect
+{
+    class ServiceConnectService
+    {
+    }
+}
