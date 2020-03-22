7456b4fcf61ba6be2b30d2718cd081c0327d2f59
zhaoliang <185653517@qq.com>
2020-03-22 23:05:01 +0800

    Add        CustomerService/src/AfarsoftResourcePlan.Core/OauthLogin/OauthSetting.cs

---
+++ CustomerService/src/AfarsoftResourcePlan.Core/OauthLogin/OauthSetting.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfarsoftResourcePlan.OauthLogin
+{
+    public class OauthSetting : Entity<int>
+    {
+        /// <summary>
+        /// 第三方平台名称
+        /// </summary>
+        public string ThirdPlatName { get; set; }
+        /// <summary>
+        /// 第三方平台编码
+        /// </summary>
+        public string ThirdPlatCode { get; set; }
+        /// <summary>
+        /// 获取第三方平台授权码
+        /// </summary>
+        public string GetCodeUrl { get; set; }
+        /// <summary>
+        /// 回调地址
+        /// </summary>
+        public string RedirectUrl { get; set; }
+
+        public string AppId { get; set; }
+
+        public string Secret { get; set; }
+    }
+}
