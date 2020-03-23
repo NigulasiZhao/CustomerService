e1e196e9f8c85814dbfce84b4973803178aefe03
zhaoliang <185653517@qq.com>
2020-03-23 10:53:05 +0800

    已修改        CustomerService/src/AfarsoftResourcePlan.Application/AfarsoftResourcePlan.Application.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/IOAuthAccountService.cs 已修改        CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/OAuthAccountService.cs 已修改        CustomerService/src/AfarsoftResourcePlan.Core/AfarsoftResourcePlan.Core.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.EntityFrameworkCore/AfarsoftResourcePlan.EntityFrameworkCore.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.Migrator/AfarsoftResourcePlan.Migrator.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.Web.Core/AfarsoftResourcePlan.Web.Core.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.Web.Host/AfarsoftResourcePlan.Web.Host.csproj 已修改        CustomerService/src/AfarsoftResourcePlan.Web.Mvc/AfarsoftResourcePlan.Web.Mvc.csproj 已修改        CustomerService/test/AfarsoftResourcePlan.Tests/AfarsoftResourcePlan.Tests.csproj

--- CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/IOAuthAccountService.cs
+++ CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/IOAuthAccountService.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Abp.Application.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AfarsoftResourcePlan.OAuthUserService.OAuthCRMService
 {
-    class IOAuthAccountService
+    public interface IOAuthAccountService : IApplicationService
     {
+
     }
 }
--- CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/OAuthAccountService.cs
+++ CustomerService/src/AfarsoftResourcePlan.Application/OAuthUserService/OAuthCRMService/OAuthAccountService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using AfarsoftResourcePlan.OauthLogin;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace AfarsoftResourcePlan.OAuthUserService.OAuthCRMService
 {
-    class OAuthAccountService
+    public class OAuthAccountService : ApplicationService, IOAuthAccountService
     {
+        private readonly IRepository<OauthSetting, int> _oauthSetting;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OAuthAccountService(IRepository<OauthSetting, int> oauthSetting, IHttpClientFactory httpClientFactory)
+        {
+            _oauthSetting = oauthSetting;
+            _httpClientFactory = httpClientFactory;
+        }
+        public string GetOauthLoginUrl(string ThirdPlatCode)
+        {
+            OauthSetting OauthSettingModel = _oauthSetting.FirstOrDefault(e => e.ThirdPlatCode == ThirdPlatCode);
+            if (OauthSettingModel != null)
+            {
+
+            }
+            return "";
+        }
     }
 }
