67e9c12c4bb0e7dcf845b89a927f0aa78b3d6866
zhaoliang <185653517@qq.com>
2020-03-23 14:48:05 +0800

    已修改        CustomerService/src/AfarsoftResourcePlan.EntityFrameworkCore/Migrations/AfarsoftResourcePlanDbContextModelSnapshot.cs Add        CustomerService/src/AfarsoftResourcePlan.EntityFrameworkCore/Migrations/20200323064714_zl032302.Designer.cs Add        CustomerService/src/AfarsoftResourcePlan.EntityFrameworkCore/Migrations/20200323064714_zl032302.cs

---
+++ CustomerService/src/AfarsoftResourcePlan.EntityFrameworkCore/Migrations/20200323064714_zl032302.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AfarsoftResourcePlan.Migrations
+{
+    public partial class zl032302 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RedirectUrl",
+                table: "OauthSetting",
+                newName: "AuthorizationUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuthorizationUrl",
+                table: "OauthSetting",
+                newName: "RedirectUrl");
+        }
+    }
+}
