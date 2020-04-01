de0101fa026c4607ec1bca6ca6b0ebcf43edf368
zhaoliang <185653517@qq.com>
2020-04-01 16:57:22 +0800

    已修改        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/ChatRecordsService.cs 已修改        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/IChatRecordsService.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/Dto/HistoryChatRecordsListInput.cs Add        CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/Dto/HistoryChatRecordsListOutput.cs

---
+++ CustomerService/src/AfarsoftResourcePlan.Application/CRMCustomerService/CRMChatRecords/Dto/HistoryChatRecordsListInput.cs
@@ -0,0 +1,12 @@
+﻿using AfarsoftResourcePlan.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfarsoftResourcePlan.CRMCustomerService.CRMChatRecords.Dto
+{
+    public class HistoryChatRecordsListInput : BasePageInput
+    {
+        public string ServiceId { get; set; }
+    }
+}
